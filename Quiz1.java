be3912a38489bf7aaaa602c8b1a2a8ba14b235be
Muskan. <83203071+MuskanValmiki@users.noreply.github.com>
2025-09-06 01:49:26 +0530

    Add files via upload

---
+++ Quiz1.java
@@ -0,0 +1,38 @@
+public class Quiz1 {
+    public static void main(String arg[]){
+        //Q1.Which method is used to add a new line character at the end of the output in the console?
+        //println()
+        //Q2.Is Java a Strictly Typed Language?
+        //Yes
+        //Q3.What kind of error will be thrown if the main method is not found in the Java program?
+        //Runtime error
+        //Q4.Which of the following is the valid way to represent a long literal in Java?
+        //123L, 123l, 0b1010L
+        //Q5.What do you mean by type promotion in Java?
+        //Conversion of data from lower to higher data type
+        //Q6.Choose an invalid way to declare a character variable in Java.
+        //char c="a";
+        //Q7.What is the default value assigned to a variable of boolean type?
+        //false
+        //Q8.What is the result of the following code snippet?
+        //class Main{
+        //     public static void main(String []args)
+        //               char ch='a';
+        //               char result=(char)(ch+1);
+        //                System.out.println(result);
+        //     }
+        //}
+        //b
+        //Q9.What is the result of the following code snippet?
+        //class Main{
+        //       public static void main (String []args){
+        //             byte b1=5,b2=6;
+        //             byte b3=(byte)(b1+b2);
+        //             System.out.println(b3);
+        //        }
+        //}
+        //11
+        //Q10.What does a high-order bit represent for an integer?
+        //sign of the integer
+    }
+}
---
+++ Quiz2.java
@@ -0,0 +1,14 @@
+public class Quiz2 {
+    public static void main(String args[]){
+        int a = 20;
+         int b = 10;
+         System.out.println("Addition: "+ (a+b));
+         System.out.println("Subtraction: "+ (a-b));
+         System.out.println("Multiplication: "+ (a*b));
+         System.out.println("Division: "+ (a/b));
+         boolean isGreater = a > b;
+         boolean logicalCheck = a > b && b > 0;
+         System.out.println("Is a greater than b? " + isGreater);
+         System.out.println("Is a > b and b > 0? " + logicalCheck);
+    }
+}
